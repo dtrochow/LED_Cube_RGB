53942d350e0f68061dfdde9d843afa8655461b24
Dominik Trochowski <dominik.trochowski@gmail.com>
2022-06-06 00:22:05 +0200

    RLC-3: improve building scripts

--- build_utests.sh
+++ build_utests.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-cd unit
-cmake -S . -B build
-cmake --build build
+function build {
+    cd unit
+    cmake -S . -B build
+    cmake --build build
+}
+
+if ! build ; then
+    echo Build failed!
+fi
---
+++ clean_utest_build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -rf unit/build/
+mkdir unit/build
