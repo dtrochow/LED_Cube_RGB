24b385effb97be00a27fe32f819c2c47741c500d
Dominik Trochowski <dominik.trochowski@gmail.com>
2022-06-07 23:24:31 +0200

    RLC-3: Move types to led_rgb_types.hpp file

--- new_led_cube/include/led_rgb.hpp
+++ new_led_cube/include/led_rgb.hpp
@@ -1,36 +1,6 @@
+#include "led_rgb_types.hpp"
+
 #pragma once
-
-enum class Color : std::uint8_t {
-    NONE,
-    RED,
-    GREEN,
-    BLUE,
-    CYAN,
-    MAGENTA,
-    YELLOW,
-    WHITE
-};
-
-enum class Led : std::uint8_t {
-    RED,
-    GREEN,
-    BLUE
-};
-
-enum class LedState : bool {
-    DISABLED,
-    ENABLED
-};
-
-struct Led_t {
-    LedState red;
-    LedState green;
-    LedState blue;
-
-    bool operator==(const Led_t& rh) const {
-        return std::make_tuple( this->red, this->green, this->blue ) == std::make_tuple( rh.red, rh.green, rh.blue );
-    }
-};
 
 class LedColor {
 public:
