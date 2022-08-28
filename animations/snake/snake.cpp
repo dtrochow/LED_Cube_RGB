eb6175dde376a27bb8c79bd17e4ad963fd9444e9
dtrochow <dominik.trochowski@gmail.com>
2022-08-28 15:08:31 +0000

    Rebase Snake animation

--- animations/snake/snake.cpp
+++ animations/snake/snake.cpp
@@ -1,19 +1,24 @@
 #include <algorithm>
 #include "snake.hpp"
 
+Snake::Snake() {
+    next_color = get_random_color();
+}
+
 /**
  * Snake animation
  */
 void Snake::run(LedCube* cube, AnimationSpeed speed, int iterations) {
     get_cube_dimensions(cube, &cube_dimensions);
     one_frame_time_us = 300000/(int64_t)speed;
+    init_action_matrix();
 
     dir = drawFirstDirection();
     drawStartingPoint();
 
     while (true) {
         last_dir = dir;
-        last_pos = allocated_diodes.back();
+        last_pos = allocated_diodes.front().first;
 
         if (isHitTheWall() || isChangeDirection()) {
             auto possible_dirs = getAllCurrentPossibleDirs();
@@ -25,6 +30,18 @@ void Snake::run(LedCube* cube, AnimationSpeed speed, int iterations) {
         moveSnake(dir);
         render(cube);
     };
+}
+
+void Snake::init_action_matrix() {
+    action_matrix.resize(cube_dimensions.x, std::vector<vector<MatrixOperation*>>(cube_dimensions.y, std::vector<MatrixOperation*>(cube_dimensions.z, nullptr)));
+    action_matrix[1][1][1] = new EnableSingle(new CartesianCoordinates(1,1,1));
+    for (int x = 0; x < cube_dimensions.x; x++) {
+        for (int y = 0; y < cube_dimensions.y; y++) {
+            for (int z = 0; z < cube_dimensions.z; z++) {
+                action_matrix[x][y][z] = new EnableSingle(new CartesianCoordinates(x, y, z));
+            }
+        }
+    }
 }
 
 bool Snake::isHitTheWall() {
@@ -43,7 +60,7 @@ bool Snake::isChangeDirection() {
 
 void Snake::drawStartingPoint() {
     start_pos = get_random_pos(cube_dimensions);
-    allocated_diodes.push_back({start_pos.x, start_pos.y, start_pos.z});
+    allocated_diodes.push_back({{start_pos.x, start_pos.y, start_pos.z}, next_color});
     sleep_ms(one_frame_time_us/1000);
 }
 
@@ -77,11 +94,10 @@ std::vector<Direction> Snake::getAllCurrentPossibleDirs() {
     // Remove possibility of running into the snake tail
     for (auto it_dir = allowed_dir.begin(); it_dir != allowed_dir.end();) {
         potential_next_pos = last_pos + get_direction(*it_dir);
-        if (std::count(allocated_diodes.begin(), allocated_diodes.end(), potential_next_pos)) {
-            it_dir = allowed_dir.erase(it_dir);
-        } else {
-            it_dir++;
-        }
+        bool if_intersect = (std::find_if(allocated_diodes.begin(), allocated_diodes.end(), 
+                             [&potential_next_pos](const std::pair<cartesianPos_t, Color> &diode)
+                             {return potential_next_pos == diode.first;}) != allocated_diodes.end());
+        if_intersect ? it_dir = allowed_dir.erase(it_dir) : it_dir++;
     }
     return allowed_dir;
 }
@@ -95,30 +111,25 @@ Direction Snake::drawDirection(std::vector<Direction> dirs) {
 
 void Snake::moveSnake(Direction dir) {
     cartesianPos_t next_pos = last_pos + get_direction(dir);
-    allocated_diodes.push_back(next_pos);
+    allocated_diodes.insert(allocated_diodes.begin(), {next_pos, next_color});
     if (allocated_diodes.size() > snake_length) {
-        allocated_diodes.erase(allocated_diodes.begin());
+        allocated_diodes.erase(allocated_diodes.end());
+    }
+    snake_move_counter++;
+    if (snake_move_counter % color_segment_length == 0) {
+        next_color = get_random_color();
     }
 }
 
 void Snake::render(LedCube* cube) {
     absolute_time_t start = get_absolute_time();
     while (absolute_time_diff_us(start, get_absolute_time()) < one_frame_time_us) {
-        int tail_pos = 0;
-        auto it_color = all_colors.begin();
         for (auto it = allocated_diodes.begin(); it != allocated_diodes.end(); it++) {
-            cartesianPos_t pos = *it;
-            CartesianCoordinates cc(pos.x, pos.y, pos.z);
-            EnableSingle single(&cc);
-            cube->action(&single, LedSwitch::ENABLE, *it_color);
+            cartesianPos_t pos = it->first;
+            cube->action(action_matrix[pos.x][pos.y][pos.z], LedSwitch::ENABLE, it->second);
             cube->render();
-            cube->action(&single, LedSwitch::DISABLE);
+            cube->action(action_matrix[pos.x][pos.y][pos.z], LedSwitch::DISABLE);
             cube->render();
-            tail_pos++;
-            // Dividing the tail into separate color sections
-            if (tail_pos % color_segment_length == 0) {
-                it_color++;
-            }
         }
-    }; 
+    };
 }
